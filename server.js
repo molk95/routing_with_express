55d5a136fc1073ea40d431bfdedfd2f3361d06a9
molk95 <molk.saouabi@gmail.com>
2020-01-09 15:29:34 +0100

    first commit in express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+let hour_now = new Date().getHours();
+
+app.get('/', function(req, res) {
+  if ( (hour_now>8)&&(hour_now<17) ) res.sendFile(__dirname + '/public/home.html');
+  else res.sendFile(__dirname + '/public/outOfService.html');
+})
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(3000, err => {
+  if (err) console.log("server is not running");
+  else console.log("server is running on port 3000");
+});
